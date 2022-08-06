595e13f720d8c7c53839bb5d6fd7030a52862811
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-08-06 06:04:36 +0600

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min + 1;
+        this.max = max;
     }
 
     guess() {
-
+        this.lastGuess = Math.floor((this.min + this.max) / 2);
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.max = this.lastGuess;
     }
 
     greater() {
-
+        this.min = this.lastGuess + 1;
     }
 }
 
